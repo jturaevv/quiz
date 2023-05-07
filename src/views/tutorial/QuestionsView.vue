6f14416275ccb29a3d573d506cfccadda295f0c5
jorabek <b1903357@gmail.com>
2023-05-08 01:11:45 +0500

    changes in question page

--- src/views/tutorial/QuestionsView.vue
+++ src/views/tutorial/QuestionsView.vue
@@ -1,23 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import questionApp from './questionApp.vue'
+import resultApp from './resultApp.vue'
+</script>
 
 <template>
   <div class="questions-page page">
     <div class="container">
       <div class="questions-wrapper">
-        <h2 class="questions__title">
-          Но реплицированные с зарубежных источников, современные исследования
-          набирают популярность среди?
-        </h2>
-        <div class="questions-body">
-          <div class="questions-block" v-for="item in 4" :key="item">
-            <h5 class="questions__option">A</h5>
-            <p class="questions__text">
-              Современные технологии достигли такого уровня, что убеждённость
-              некоторых оппонентов говорит о возможностях экспериментов,
-              поражающих по своей масштабности и грандиозности.
-            </p>
-          </div>
-        </div>
+        <!-- <question-app /> -->
+        <resultApp />
       </div>
     </div>
   </div>
@@ -42,51 +33,6 @@
     display: flex;
     flex-direction: column;
     gap: 20px;
-  }
-
-  &-block {
-    display: flex;
-    align-items: center;
-    padding: 25px 37px;
-    gap: 37px;
-    background: #e9f2f5;
-    border: 2px solid #dce9e9;
-    border-radius: 20px;
-    cursor: pointer;
-    transition: all 0.3s;
-
-    &:hover {
-      background: #ffd9cb;
-      border: 2px solid #eb6440;
-      border-radius: 20px;
-
-      .questions__option {
-        color: #e9f2f5;
-        background: #eb6440;
-        border: 2px solid #eb6440;
-        border-radius: 37.037px;
-        transition: all 0.3s;
-      }
-    }
-  }
-
-  &__option {
-    font-weight: 700;
-    font-size: 18px;
-    line-height: 130%;
-    text-align: center;
-    padding: 8px 13px;
-    border: 2px solid #060c0e;
-    border-radius: 37.037px;
-    color: #060c0e;
-  }
-
-  &__text {
-    font-family: "Inter";
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 130%;
-    color: #060c0e;
   }
 }
 </style>
---
+++ src/views/tutorial/resultApp.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2 class="result__title">Ваш результат</h2>
+  <div class="result-body">
+    <figure class="pie-chart">
+      <h3 class="caption-inside">71%</h3>
+    </figure>
+  </div>
+</template>
+
+<style lang="scss">
+.pie-chart {
+  background: radial-gradient(
+      circle closest-side,
+      white 0,
+      white 52.8%,
+      transparent 52.8%,
+      transparent 60%,
+      #eff5f5 0
+    ),
+    conic-gradient(#203d48 92%, #203d48 83%, #a9ccd9 75%, #a9ccd9 100%);
+  position: relative;
+  width: 500px;
+  min-height: 350px;
+}
+.pie-chart .caption-inside {
+  font-weight: 700;
+  font-size: 48px;
+  line-height: 130%;
+  color: #000000;
+  position: absolute;
+  top: 50%;
+  transform: translate(50%, -50%);
+  left: 33%;
+}
+</style>
